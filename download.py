65aecaacb7c11566c15a2208d4df3739c7e233f1
Konstas Marmatakis <marmako@gmail.com>
2020-01-21 00:53:32 +0200

    Added license in files.

--- download.py
+++ download.py
@@ -1,3 +1,22 @@
+###########################################################################
+# KPG Past Exams Files Downoload
+#    Copyright (C) <2020>  <Konstas Marmatakis> <email:marmako@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###########################################################################
+
+
 import os
 import sys
 import requests
@@ -5,6 +24,12 @@ from kpglinks import languages
 
 
 def download_files(*linkgroups):
+    """
+    Downloads the practice papers from past exams KPG.
+    linkgroups: The language or languages who files downloads.
+    Example: download_files('german')
+             download_files('spanish', 'english')
+    """
     for group in linkgroups:
         try:
             grouplist = languages[group]
