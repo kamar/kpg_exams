2b22358f2841f4a8ca0aad16b4f3fa45be010b9d
Konstas Marmatakis <marmako@gmail.com>
2020-01-21 17:47:37 +0200

    Added links.

--- kpglinks.py
+++ kpglinks.py
@@ -100,5 +100,21 @@ languages = {
     "https://www.minedu.gov.gr/publications/kpgdocs/2019a/2019a_esp_b_audio.mp3", 
     "https://www.minedu.gov.gr/publications/kpgdocs/2019a/2019a_esp_c_text.zip", 
     "https://www.minedu.gov.gr/publications/kpgdocs/2019a/2019a_esp_c_audio.mp3", 
-    "https://rcel2.enl.uoa.gr/kpg/files/past_papers/spanish/2014-05/ES_B_MAY14.zip"]
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018b/2018b_esp_b_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018b/2018b_esp_b_audio.mp3",
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018b/2018b_esp_c_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018b/2018b_esp_c_audio.mp3", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_a_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_a_audio.mp3", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_b_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_b_audio.mp3", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_c_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2018a/2018a_esp_c_audio.mp3", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2017b/2017b_esp_b_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2017b/2017b_esp_b_audio.mp3", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2017b/2017b_esp_c_text.zip", 
+    "https://www.minedu.gov.gr/publications/kpgdocs/2017b/2017b_esp_c_audio.mp3", 
+    "https://rcel2.enl.uoa.gr/kpg/files/past_papers/spanish/2014-05/ES_B_MAY14.zip", 
+    "https://rcel2.enl.uoa.gr/kpg/files/past_papers/spanish/2014-05/ES_C_MAY14.zip", 
+    "https://rcel2.enl.uoa.gr/kpg/files/past_papers/spanish/2014-05/ES_A_MAY14.zip"]
 }
